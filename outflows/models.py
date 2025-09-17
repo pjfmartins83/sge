f0fe78deaeaf896b3c87d72778347d0b9e4acb1b
Paulo Martins <pjfmartins83@gmail.com>
2025-09-17 12:51:16 -0300

    17/09/2025

--- outflows/models.py
+++ outflows/models.py
@@ -3,9 +3,7 @@ from products.models import Product
 
 
 class Outflow(models.Model):
-    product = models.ForeignKey(
-        Product, on_delete=models.PROTECT, related_name="outflows"
-    )
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name="outflows")
     quantity = models.IntegerField()
     description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- products/models.py
+++ products/models.py
@@ -5,9 +5,7 @@ from brands.models import Brand
 
 class Product(models.Model):
     title = models.CharField(max_length=500)
-    category = models.ForeignKey(
-        Category, on_delete=models.PROTECT, related_name="products"
-    )
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name="products")
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name="products")
     description = models.TextField(null=True, blank=True)
     serie_number = models.CharField(max_length=200, null=True, blank=True)
