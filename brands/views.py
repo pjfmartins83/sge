39bf6b3f7f8c5c4f1d321dbee2220a238d4aeb9c
Paulo Martins <pjfmartins83@gmail.com>
2025-09-19 12:34:47 -0300

    brands finished

--- brands/views.py
+++ brands/views.py
@@ -1,17 +1,44 @@
-from django.views.generic import ListView
-from . import models
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from . import models, forms
 
 
 class BrandListView(ListView):
     model = models.Brand
     template_name = "brand_list.html"
     context_object_name = "brands"
+    paginate_by = 10
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        name = self.request.GET.get("name")
+        search = self.request.GET.get("search")
 
-        if name:
-            queryset = queryset.filter(name__icontains=name)
+        if search:
+            queryset = queryset.filter(name__icontains=search)
 
         return queryset
+
+
+class BrandCreateView(CreateView):
+    model = models.Brand
+    template_name = "brand_create.html"
+    form_class = forms.BrandForm
+    success_url = reverse_lazy("brand_list")
+
+
+class BrandDetailView(DetailView):
+    model = models.Brand
+    template_name = "brand_detail.html"
+
+
+class BrandUpdateView(UpdateView):
+    model = models.Brand
+    template_name = "brand_update.html"
+    form_class = forms.BrandForm
+    success_url = reverse_lazy("brand_list")
+
+
+class BrandDeleteView(DeleteView):
+    model = models.Brand
+    template_name = "brand_delete.html"
+    success_url = reverse_lazy("brand_list")
