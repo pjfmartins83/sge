6e0e4b7fbe747eefe0c42b7b387da83e3ecaeccc
Paulo Martins <pjfmartins83@gmail.com>
2025-09-22 14:39:15 -0300

    crud inflows & outflows

--- inflows/admin.py
+++ inflows/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class InflowAdmin(admin.ModelAdmin):
+    list_display = [
+        "supplier",
+        "product",
+        "quantity",
+        "created_at",
+        "updated_at",
+    ]
+    search_fields = [
+        "suppler__name",
+        "product__title",
+    ]
+
+
+admin.site.register(models.Inflow, InflowAdmin)
